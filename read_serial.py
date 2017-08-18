d7c04265e389e3468bdcd1ebf6599ca4dc37068f
axelinternet <axel@hultman.info>
2017-08-19 00:20:00 +0200

    Basic setup to send card UID to Firebase

---
+++ read_serial.py
@@ -0,0 +1,45 @@
+import serial
+import requests
+import time
+import json
+import firebase_admin
+from database_info import DATABASE_URL
+from firebase_admin import db
+from firebase_admin import credentials
+
+cred = credentials.Certificate("secrets.json")
+options = {"databaseURL": DATABASE_URL}
+default_app = firebase_admin.initialize_app(cred, options)
+ref = db.reference("path")
+
+timeout = {}
+
+def detect_serial_input(): 
+    with serial.Serial('/dev/ttyACM0', 9600) as ser:
+        timeout = []
+        while True:
+            s = ser.readline() #Change this to a better format. Read stream and detect when UI string comes.
+            if (s.decode()[0:6] != "Card U"): #Detect card UID from exampel GetInfo program.
+                s = ser.readline()
+            else: 
+                uid = s.decode()[10:21]
+                send_to_firebase()
+
+def send_to_firebase(uid):
+    if uid not in timeout or timeout[uid] - time.time() > 10.0: 
+        timeout[uid] = time.time()
+        # SEND TO WEB HERE
+        print("Sending to firebase")
+        ref.push({
+            "uid": uid,
+            "time": time.time()
+            })
+    else:
+        print("Timeout for ", uid)
+
+if __name__ == "__main__":
+    #detect_serial_input()
+    send_to_firebase("FF AA BB CC")   
+    send_to_firebase("FF AA BB CC")   
+    send_to_firebase("AA BA BB CC")   
+    print(timeout)
