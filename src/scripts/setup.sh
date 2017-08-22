5e877a5092e334874de0650ab6194addd88c4e71
axelinternet <axel@hultman.info>
2017-08-22 14:51:07 +0200

    added a basic setup script that needs to be updated later when more videos are added

---
+++ src/scripts/setup.sh
@@ -0,0 +1,7 @@
+echo "Setting up complex shit"
+cp ../index.html ../../dist/index.html
+wget http://medielabbet.nu/pics/c1r2.mp4 
+echo "Downloaded c1r2.mp4"
+wget http://medielabbet.nu/pics/c1r3.mp4
+echo "Downloaded c1r3.mp4"
+mv c*r*.mp4 ../../dist/
