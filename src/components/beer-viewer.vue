77523d381621b8cfa0b138a1b4e126aba6418db5
mfrisell <mfrisell@kth.se>
2017-08-22 21:08:36 +0200

    lots of stuff

--- src/components/beer-viewer.vue
+++ src/components/beer-viewer.vue
@@ -28,19 +28,98 @@ const beerViewer = {
 	template: beerViewerTemplate,
  	data: () => {
  		return {
- 			header: "Cool360"
+ 			header: "Cool360",
+      beerHeight: '0%',
+      beerCheckInsObject: {},
+      topTen: []
  		}
  	},
- 	firebase: {
- 		beers: beerRef
- 	},
- 	methods: {
- 		listen: () => {
- 			this.$firebaseRefs.beers.on('value', function(snapshot) {
-      	console.log(snapshot.val())
-    	})
+  firebase: {
+ 		 beers: {
+      source: beerRef,
+      // optionally bind as an object
+      asObject: false,
+      // optionally provide the cancelCallback
+      cancelCallback: function () {},
+      // this is called once the data has been retrieved from firebase
+      readyCallback: function (banana) {
+      	const snap = this.$firebaseRefs.beers.on('value', function(snapshot) {
+          // this.data.beerCheckIns = snapshot.val();
+      		// console.log(snapshot.val())
+          const snip = snapshot.val();
+          return snip
+      	})
+        this.beerCheckInsObject = snap(banana)
+        //console.log(this.beerCheckIns)
+
+        // Konvertera Object till Array, räkna förekomst. .
+        const checkIns = this.beerCheckInsObject
+        const checkInsArr = Object.keys(checkIns).map(function (key) {
+          return checkIns[key];
+        });
+
+        const totalAmountBeers = 500;
+        const amountBeers = checkInsArr.length;
+        // const amountBeers = 250;
+        const beerPerPixel = 100/totalAmountBeers * 10;
+
+        this.beerHeight = Math.round(amountBeers*beerPerPixel) + "%";
+
+        const result = {};
+        checkInsArr.forEach(function(i) {
+          if (!result.hasOwnProperty(i.uid)) {
+            result[i.uid] = 0;
+          }
+          result[i.uid] +=1;
+        });
+
+        const newArrayA = Object.keys(result).map(function(k) {
+          // const o={};
+          // o[k] = result[k];
+          const o = {'uid':k, 'amount':result[k]}
+          return o;
+        })
+
+        newArrayA.sort(function(a, b) {
+          return b.amount - a.amount;
+        });
+
+        this.topTen = newArrayA
+        console.log(this.topTen)
+
+      }
     }
- 	}
+ 	},
+  updated: function() {
+
+      // // Konvertera Object till Array, räkna förekomst. .
+      // const checkIns = this.beerCheckInsObject
+      // const checkInsArr = Object.keys(checkIns).map(function (key) {
+      //   return checkIns[key];
+      // });
+      //
+      // const result = {};
+      // checkInsArr.forEach(function(i) {
+      //   if (!result.hasOwnProperty(i.uid)) {
+      //     result[i.uid] = 0;
+      //   }
+      //   result[i.uid] +=1;
+      // });
+      //
+      // const newArrayA = Object.keys(result).map(function(k) {
+      //   // const o={};
+      //   // o[k] = result[k];
+      //   const o = {'uid':k, 'amount':result[k]}
+      //   return o;
+      // })
+      //
+      // newArrayA.sort(function(a, b) {
+      //   return b.amount - a.amount;
+      // });
+      //
+      // this.topTen = newArrayA
+      // console.log(this.topTen)
+  }
 }
 
 export default beerViewer
